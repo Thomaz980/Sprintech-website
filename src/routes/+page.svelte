8c55ae5254e01409fec4f95f4c65d1c77db35d84
Thomaz980 <thomazrodrigues668@gmail.com>
2023-11-19 19:08:09 -0300

    auterações realizadas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import Nav from '../components/Nav.svelte';
 	import Footer from '../components/Footer.svelte';
 	import Img from '../lib/images/ilustracao.svg';
