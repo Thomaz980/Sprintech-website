2873e390ebe78d9a19108e60baebee02b06ac94e
Thomaz980 <thomazrodrigues668@gmail.com>
2023-11-19 15:40:05 -0300

    organizando elementos

---
+++ src/components/Footer.svelte
@@ -0,0 +1,99 @@
+<script>
+    
+</script>
+<footer>
+    <span class="blur"></span>
+    <span class="blur"></span>
+    <div class="column">
+        <div class="logo">
+            <img src="#" alt="">
+        </div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            quibusdam, voluptatum.
+        </p>
+        <div class="socials">
+            <a href="#"><i class="ri-instagram-line"></i></a>
+            <a href="#"><i class=""></i></a>
+            <a href="#"><i class=""></i></a>
+        </div>
+    </div>
+    <div class="column">
+        <h4>Company</h4>
+        <a href="">Business</a>
+        <a href="">Partnership</a>
+        <a href="">Networking</a>
+    </div>
+    <div class="column">
+        <h4>About Us</h4>
+        <a href="">Business</a>
+        <a href="">Partnership</a>
+        <a href="">Networking</a>
+    </div>
+    <div class="column">
+        <h4>Contect</h4>
+        <a href="">Business</a>
+        <a href="">Partnership</a>
+        <a href="">Networking</a>
+    </div>
+</footer>
+
+<style>
+
+footer {
+	position: relative;
+	display: grid;
+	padding: 5rem 2rem;
+	grid-template-columns: repeat(3, 1fr);
+	gap: 2rem;
+}
+
+footer .column .logo {
+	max-width: 100px;
+	margin-bottom: 2rem;
+}
+
+footer .column p {
+	color: #ccc;
+	margin-bottom: 2rem;
+}
+
+footer .column .socials {
+	display: flex;
+	align-items: center;
+	gap: 1rem;
+}
+
+footer .column .socials a {
+	color: #fff;
+	font-size: 1.5rem;border: 1px solid #ccc;
+	padding: 5px 10px;
+	font-size: 1.25rem;
+	border-radius: 100%;
+	transition: all 0.3s ease;
+}
+
+footer .column .socials a:hover {
+	color: #fff;
+	background-color: #1d4ed8;
+	border-color: #1d4ed8;
+}
+
+footer .column h4 {
+	color: #fff;
+	margin-bottom: 2rem;
+	font-size: 1.2rem;
+	font-weight: 500;
+}
+
+footer .column > a {
+	color: #ccc;
+	margin-bottom: 1rem;
+	display: block;
+	transition: all 0.3s ease;
+}
+
+footer .column > a:hover {
+	color: #1d4ed8;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import Nav from '../components/Nav.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <svelte:head>
@@ -17,12 +17,10 @@
 		<div class="content">
 			<span class="blur"></span>
 			<span class="blur"></span>
-			<h4>CREATE YOUR SITE A PRO</h4>
-			<h1><span>Sprintech</span>, Desenvolvimento de Software</h1>
+			<h4>CREATE YOUR SITE</h4>
+			<h1><span>Sprintech</span>, Desenvolvimento Web</h1>
 			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-				quibusdam, voluptatum, quos, voluptate quia quae dolorum
-				accusantium.
+				Seu negócio é o nosso negócio.
 			</p>
 			<button class="btn">Get Started</button>
 		</div>
@@ -32,11 +30,11 @@
 	</header>
 
 	<section class="conteiner">
-		<h2 class="header">OUR FEATURES</h2>
+		<h2 class="header">FEATURES</h2>
 		<div class="features">
 			<div class="card">
 				<span><i class="icon"></i></span>
-				<h4>Free Tutorials</h4>
+				<h4>Card #1</h4>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
 					quibusdam, voluptatum, quos, voluptate quia quae dolorum
@@ -46,7 +44,7 @@
 			</div>
 			<div class="card">
 				<span><i class="icon"></i></span>
-				<h4>Free Tutorials</h4>
+				<h4>Card #2</h4>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
 					quibusdam, voluptatum, quos, voluptate quia quae dolorum
@@ -56,7 +54,7 @@
 			</div>
 			<div class="card">
 				<span><i class="icon"></i></span>
-				<h4>Free Tutorials</h4>
+				<h4>Card #3</h4>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
 					quibusdam, voluptatum, quos, voluptate quia quae dolorum
@@ -69,42 +67,7 @@
 
 	<!-- Contato -->
 
-	<footer>
-		<span class="blur"></span>
-		<span class="blur"></span>
-		<div class="column">
-			<div class="logo">
-				<img src="#" alt="">
-			</div>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-				quibusdam, voluptatum.
-			</p>
-			<div class="socials">
-				<a href="#"><i class="ri-instagram-line"></i></a>
-				<a href="#"><i class=""></i></a>
-				<a href="#"><i class=""></i></a>
-			</div>
-		</div>
-		<div class="column">
-			<h4>Company</h4>
-			<a href="">Business</a>
-			<a href="">Partnership</a>
-			<a href="">Networking</a>
-		</div>
-		<div class="column">
-			<h4>About Us</h4>
-			<a href="">Business</a>
-			<a href="">Partnership</a>
-			<a href="">Networking</a>
-		</div>
-		<div class="column">
-			<h4>Contect</h4>
-			<a href="">Business</a>
-			<a href="">Partnership</a>
-			<a href="">Networking</a>
-		</div>
-	</footer>
+	<Footer />
 
 	<div class="copyright">
 		<p>&copy; 2021 Sprintech. All Rights Reserved.</p>
@@ -273,62 +236,6 @@ section .header {
 	color: #ccc;
 }
 
-footer {
-	position: relative;
-	display: grid;
-	padding: 5rem 2rem;
-	grid-template-columns: repeat(3, 1fr);
-	gap: 2rem;
-}
-
-footer .column .logo {
-	max-width: 100px;
-	margin-bottom: 2rem;
-}
-
-footer .column p {
-	color: #ccc;
-	margin-bottom: 2rem;
-}
-
-footer .column .socials {
-	display: flex;
-	align-items: center;
-	gap: 1rem;
-}
-
-footer .column .socials a {
-	color: #fff;
-	font-size: 1.5rem;border: 1px solid #ccc;
-	padding: 5px 10px;
-	font-size: 1.25rem;
-	border-radius: 100%;
-	transition: all 0.3s ease;
-}
-
-footer .column .socials a:hover {
-	color: #fff;
-	background-color: #1d4ed8;
-	border-color: #1d4ed8;
-}
-
-footer .column h4 {
-	color: #fff;
-	margin-bottom: 2rem;
-	font-size: 1.2rem;
-	font-weight: 500;
-}
-
-footer .column > a {
-	color: #ccc;
-	margin-bottom: 1rem;
-	display: block;
-	transition: all 0.3s ease;
-}
-
-footer .column > a:hover {
-	color: #1d4ed8;
-}
 
 .copyright {
 	max-width: 1200px;
